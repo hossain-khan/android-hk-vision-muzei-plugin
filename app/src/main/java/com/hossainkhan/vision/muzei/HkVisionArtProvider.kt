c0f5f9310387e467ce1fa6fffb6577f4720a8e4c
Hossain Khan <amardeshbd@gmail.com>
2020-05-10 00:06:38 -0400

    [ADDED] Link to the photo provider.

--- app/src/main/java/com/hossainkhan/vision/muzei/HkVisionArtProvider.kt
+++ app/src/main/java/com/hossainkhan/vision/muzei/HkVisionArtProvider.kt
@@ -1,9 +1,11 @@
 package com.hossainkhan.vision.muzei
 
 import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
+import com.hossainkhan.vision.data.HkVisionWorker
 
 class HkVisionArtProvider : MuzeiArtProvider() {
     override fun onLoadRequested(initial: Boolean) {
-        TODO("Not yet implemented")
+        val context = context ?: return
+        HkVisionWorker.enqueueLoad(context)
     }
 }
