d6a11a9dea042569223d1713f37b4fa9889896ee
Hossain Khan <amardeshbd@gmail.com>
2020-05-23 23:10:26 -0400

    [ADDED] Crashlytics to monitor performance.

--- app/src/main/java/com/hossainkhan/vision/muzei/HkVisionArtProvider.kt
+++ app/src/main/java/com/hossainkhan/vision/muzei/HkVisionArtProvider.kt
@@ -1,11 +1,22 @@
 package com.hossainkhan.vision.muzei
 
+import android.content.Context
 import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.hossainkhan.vision.data.HkVisionWorker
 
 class HkVisionArtProvider : MuzeiArtProvider() {
     override fun onLoadRequested(initial: Boolean) {
-        val context = context ?: return
+        val context: Context? = context
+
+        if (context == null) {
+            FirebaseCrashlytics.getInstance().recordException(
+                IllegalStateException("Missing context, unable to enqueue worker to load photos.")
+            )
+            return
+        }
+
+        FirebaseCrashlytics.getInstance().log("Enqueuing ${HkVisionWorker::class.java.simpleName} task.")
         HkVisionWorker.enqueueLoad(context)
     }
 }
